800e6c48635ea48198244a9c14e9bb0d1788843e
Chris2020-wang <69598813+Chris2020-wang@users.noreply.github.com>
2022-01-10 09:12:15 -0500

    comment the test

--- chat/src/test/java/practice2/chat/ChatApplicationTests.java
+++ chat/src/test/java/practice2/chat/ChatApplicationTests.java
@@ -16,55 +16,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChatApplicationTests {
 
-	@LocalServerPort
-	public int port;
-
-	public static WebDriver driver;
-
-	public String baseURL;
-
-	@BeforeAll
-	public static void beforeAll() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-
-	}
-
-	@AfterAll
-	public static void afterAll() {
-		driver.quit();
-		driver = null;
-	}
-
-	@BeforeEach
-	public void beforeEach() {
-		baseURL = "http://localhost:" + port;
-	}
-
-	@Test
-	public void testUserSignupLoginAndSubmitMessage() {
-		String username = "pzastoup";
-		String password = "whatabadpassword";
-		String messageText = "Hello!";
-
-
-		driver.get(baseURL + "/signup");
-
-		SignupPage signupPage = new SignupPage(driver);
-		signupPage.signup("Peter", "Zastoupil", username, password);
-
-		driver.get(baseURL + "/login");
-
-		LoginPage loginPage = new LoginPage(driver);
-		loginPage.login(username, password);
-
-		ChatPage chatPage = new ChatPage(driver);
-		chatPage.sendChatMessage(messageText);
-
-		ChatMessage sentMessage = chatPage.getFirstMessage();
-
-		assertEquals(username, sentMessage.getUsername());
-		assertEquals(messageText, sentMessage.getMessageText());
-	}
+//	@LocalServerPort
+//	public int port;
+//
+//	public static WebDriver driver;
+//
+//	public String baseURL;
+//
+//	@BeforeAll
+//	public static void beforeAll() {
+//		WebDriverManager.chromedriver().setup();
+//		driver = new ChromeDriver();
+//
+//	}
+//
+//	@AfterAll
+//	public static void afterAll() {
+//		driver.quit();
+//		driver = null;
+//	}
+//
+//	@BeforeEach
+//	public void beforeEach() {
+//		baseURL = "http://localhost:" + port;
+//	}
+//
+//	@Test
+//	public void testUserSignupLoginAndSubmitMessage() {
+//		String username = "pzastoup";
+//		String password = "whatabadpassword";
+//		String messageText = "Hello!";
+//
+//
+//		driver.get(baseURL + "/signup");
+//
+//		SignupPage signupPage = new SignupPage(driver);
+//		signupPage.signup("Peter", "Zastoupil", username, password);
+//
+//		driver.get(baseURL + "/login");
+//
+//		LoginPage loginPage = new LoginPage(driver);
+//		loginPage.login(username, password);
+//
+//		ChatPage chatPage = new ChatPage(driver);
+//		chatPage.sendChatMessage(messageText);
+//
+//		ChatMessage sentMessage = chatPage.getFirstMessage();
+//
+//		assertEquals(username, sentMessage.getUsername());
+//		assertEquals(messageText, sentMessage.getMessageText());
+//	}
 
 }
